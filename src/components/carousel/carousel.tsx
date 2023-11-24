e7eaad55b26bada000a80940b4a0d42ea829e30f
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2023-11-24 17:41:58 +0000

    updated pr template for screenshots/vids

--- src/components/carousel/carousel.tsx
+++ src/components/carousel/carousel.tsx
@@ -1,5 +1,3 @@
-import { carouselLogos } from '@/assets/carouselLogos';
-
 interface CarouselImageProps {
   images: CarouselImage[];
 }
@@ -12,14 +10,14 @@ export type CarouselImage = {
 export const Carousel = ({ images }: CarouselImageProps) => {
   console.log(images);
   return (
-    <div className='px-16'>
-      <div className='flex flex-row overflow-scroll w-full no-scrollbar scroll-smooth scrolling-auto gap-8'>
-        {images.map(logo => (
+    <div className="px-16">
+      <div className="flex flex-row overflow-scroll w-full no-scrollbar scroll-smooth scrolling-auto gap-8">
+        {images.map((logo) => (
           <img
             alt={logo.alt}
             key={logo.alt}
             src={logo.path}
-            className='transition ease-in-out grayscale opacity-25 h-20 max-w-sm hover:grayscale-0 hover:opacity-100 duration-300 h-14 max-w-sm'
+            className="transition ease-in-out grayscale opacity-25 h-20 max-w-sm hover:grayscale-0 hover:opacity-100 duration-300 h-14 max-w-sm"
           />
         ))}
       </div>
