7ecb19be491d14ba2ba4377ac2c88e638b318aa1
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2023-11-30 18:10:42 +0000

    improved tsconfig and linting

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,9 +1,10 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import { ReactElement } from "react";
 
-export const renderWithRouter = (ui: any, { route = '/' } = {}) => {
-  window.history.pushState({}, 'Test page', route);
+export const renderWithRouter = (ui: ReactElement, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
 
   return {
     user: userEvent.setup(),
