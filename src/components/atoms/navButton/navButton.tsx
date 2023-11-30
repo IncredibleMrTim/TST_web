6b5ee15983c26baff918d03588d8c64dba83f18f
Tim Smart <tim.smart@timsmarttechnology.co.uk>
2023-11-30 17:28:26 +0000

    updated navButton interface

--- src/components/atoms/navButton/navButton.tsx
+++ src/components/atoms/navButton/navButton.tsx
@@ -1,18 +1,18 @@
-import { Button, type ButtonProps } from '@material-tailwind/react';
-import { ReactElement } from 'react';
-import { useNavigate } from 'react-router-dom';
-interface LinkButtonProps extends ButtonProps {
+import { Button, type ButtonProps } from "@material-tailwind/react";
+import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
+interface NavButtonProps extends ButtonProps {
   to: string;
-  children: ReactElement;
+  children: ReactElement | string;
 }
 
-export const NavButton = ({ to, children }: LinkButtonProps) => {
+export const NavButton = ({ to, children }: NavButtonProps) => {
   const navigate = useNavigate();
 
   return (
     <Button
-      className='flex items-center gap-1.5 py-2 px-5 font-normal hover:bg-tst-primary-slate-dark'
-      role='button'
+      className="flex items-center gap-1.5 py-2 px-5 font-normal hover:bg-tst-primary-slate-dark"
+      role="button"
       onClick={() => navigate(to)}
     >
       {children}
